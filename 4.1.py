a4fa399272c9c1d8fc5b9c9f1613dbfab8bac013
Becky Rieger <brieger@ivytech.edu>
2024-10-31 14:56:48 -0500

    4.1 if else

--- 4.1.py
+++ 4.1.py
@@ -2,7 +2,7 @@ secret = 7
 guess = 3
 if guess < secret:
     print ('too low')
-elif guess > secret:
+elif guess > secret: 
     print ('too high')
 else:
-    print('just right')
+    print ('just right')
