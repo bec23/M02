9339b016aec643d195c7c1bbc7de93f1082510e9
Becky Rieger <brieger@ivytech.edu>
2024-10-31 15:40:37 -0500

    4.2 commit

---
+++ 4.2.py
@@ -0,0 +1,10 @@
+small = False
+green = True
+if small and green:
+    print ('Pea')
+elif small and not green:
+    print ('Cherry')
+elif not small and green:
+    print ('Watermelon')
+else:
+    print ('Pumpkin')
